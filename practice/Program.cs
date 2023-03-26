3ffb4a9dad080128da6da6c83b48ae9ef7ee0a2a
Evgeny <mzena002@gmail.com>
2023-03-26 23:15:47 +0300

    выполнение задания

--- practice/Program.cs
+++ practice/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arr1 = new string[5] { "145", "1123", "hi", "world", "dog" };
+string[] arr2 = new string[arr1.Length];
+
+void SecondArrayWithIF(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i].Length <= 3)
+        {
+            arr2[count] = arr1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+SecondArrayWithIF(arr1, arr2);
+PrintArray(arr2);
